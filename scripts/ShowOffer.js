d38717ca8424e27894397650098e031067838d24
Teodoro Alcantara Aquino <teodoro641@hotmail.com>
2015-05-17 18:36:15 -0400

    Page intégrée centrée au milieu de la lightbox

--- scripts/ShowOffer.js
+++ scripts/ShowOffer.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
     $('td', this).click(function(){
         //ajouté l'information a la lightbox
-        $('#lightbox-container').append('<object width="400" height="400" data="./html/elimon.html"> </object>');
+        $('#lightbox-container>object').attr('data', './html/elimon.html');
         $('#lightbox-container').css('display', 'block'); //afficher la lightbox
     });
 
     $('#lightbox-container').click(function(){
         $('#lightbox-container').css('display', 'none'); // masquer la lightbox
-        $('#lightbox-container').empty(); // supprimer enfant
-    })
+        $('#lightbox-container>object').removeAttr('data'); // supprimer l'intégré page
+    });
 });
 
 
